43fbf103808176a412344c87b6fed199647a4e64
fathulhudoyo <fathulhudoyo@animemusic.us>
2024-06-16 23:42:51 +0700

    remote not used

--- Widget-Aksesbilitas/widget_disabilitas.js
+++ Widget-Aksesbilitas/widget_disabilitas.js
@@ -748,10 +748,7 @@ var css_loader = `
     fill: white;
   }
   
-  .cursor_website_all {
-    cursor: url("data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3C!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools --%3E%3Csvg width='60px' height='60px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath style=' stroke: black; stroke-width: 2;' d='M16.5744 19.1999L12.6361 15.2616L11.4334 16.4643C10.2022 17.6955 9.58656 18.3111 8.92489 18.1658C8.26322 18.0204 7.96225 17.2035 7.3603 15.5696L5.3527 10.1205C4.15187 6.86106 3.55146 5.23136 4.39141 4.39141C5.23136 3.55146 6.86106 4.15187 10.1205 5.35271L15.5696 7.3603C17.2035 7.96225 18.0204 8.26322 18.1658 8.92489C18.3111 9.58656 17.6955 10.2022 16.4643 11.4334L15.2616 12.6361L19.1999 16.5744C19.6077 16.9821 19.8116 17.186 19.9058 17.4135C20.0314 17.7168 20.0314 18.0575 19.9058 18.3608C19.8116 18.5882 19.6077 18.7921 19.1999 19.1999C18.7921 19.6077 18.5882 19.8116 18.3608 19.9058C18.0575 20.0314 17.7168 20.0314 17.4135 19.9058C17.186 19.8116 16.9821 19.6077 16.5744 19.1999Z' fill='%23ffffff'/%3E%3C/svg%3E"), pointer !important;
-  
-  }
+
   
   .group_body_bahasa {
     display: flex;
@@ -2846,7 +2843,7 @@ $('#widget_menu_disabilitas .box_group_disabilitas').each(function (i, item) {
             }
         });
         $("#action_kursor").find(".box_text_bottom_disabilitas").removeClass('active_box_text_bottom_disabilitas');
-        $("*").removeClass("cursor_website_all");
+     
 
 
         action_space_text_widget.length = 0;
